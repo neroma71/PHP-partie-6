2e4983512723ac17ef2a6c19fb716df310c2ac0f
neroma71 <neroma71@gmail.com>
2023-06-29 11:05:51 +0200

    end of exo

--- index.php
+++ index.php
@@ -20,6 +20,22 @@
         <p>
         <label for="date">date</label>
             <input type="date" name="myDate" id="date">
+        </p>
+        <p>
+        <label for="pays">date</label>
+           <select name="pays">
+            <option value="France">France</option>
+            <option value="Angleterre">Angleterre</option>
+            <option value="Italie">Italie</option>
+           </select>
+        </p>
+        <p> 
+        <label for="building">building</label>
+            <input type="text" name="building" id="building">
+        </p> 
+        <p> 
+        <label for="room">room</label>
+            <input type="text" name="room" id="room">
         </p>    
         <input type="submit" value="envoyer">
     </form>
--- traitement.php
+++ traitement.php
@@ -1,8 +1,10 @@
 <?php
-if( !empty($_GET["lastname"]) && !empty($_GET["firstname"]) && !empty($_GET["myDate"])){
+if( !empty($_GET["lastname"]) && !empty($_GET["firstname"]) && !empty($_GET["myDate"]) && !empty($_GET["pays"])
+&& !empty($_GET["building"]) && !empty($_GET["room"])){
   
      echo "<p>bonjour ".htmlspecialchars($_GET['lastname'])." 
-     ".htmlspecialchars($_GET['firstname'])."nous sommes le".$_GET['myDate']."</p>";
+     ".htmlspecialchars($_GET['firstname'])."nous sommes le ".strftime('%d-%m-%y', strtotime(($_GET['myDate']))).
+     " en ".$_GET['pays']." immeuble ".$_GET['building']." chambre ".$_GET['room']."</p>";
     
 }else{
     echo "veuillez remplir tous les champs";
